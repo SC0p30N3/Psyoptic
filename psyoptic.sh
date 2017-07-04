d514ae7136c140de8633a9130cf395b431ae1ebe
SC0p30N3 <ccourtney@vulsec.com>
2017-07-03 22:52:09 -0400

    Cleaned code, removed Tor features for now. Updated title

--- psyoptic.sh
+++ psyoptic.sh
@@ -52,7 +52,7 @@ echo """             .   . ..   .  .      ..  .  .. ..  .
  :        :: : :       :      : :  :    :           :     :     :: :: :
  
 ========================================================================
-Created by SecEng - Cory Jay Courtney       Contact:ccourtney@vulsec.com
+Created by Security Eng. Cory Jay Courtney Contact:ccourtney@vulsec.com
 ========================================================================
 """
 #Take domain input
@@ -103,19 +103,21 @@ cewl $var > $var.wordlist.txt &)
 dig $var 
 nslookup $var) | tee $var.info.txt && cat ~/$var/passive/$var.info.txt | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq > ~/$var/passive/$var.addr.txt
 
-echo "
+echo """
 ========================================================================
-  $var has been seen using the following IP addresses:
+  $var potentially uses the following IP addresses:
 ========================================================================
-" 
+""" 
 cat ~/$var/passive/$var.addr.txt && sleep 3
-theharvester -d $var -b all | tee $var.harvest.txt
 
-#Retrieve domain info
-(dnsrecon -d $var | tee $var.dnsrecon.txt
-urlcrazy $var | tee $var.urlcrazy.txt)
+
+#Retrieve domain info and OSSINT
+
+dnsrecon -d $var | tee $var.dnsrecon.txt
+urlcrazy $var | tee $var.urlcrazy.txt
 
 #Aggriate emails and domains from the Harvester and Check for pwnage
+theharvester -d $var -b all | tee $var.harvest.txt
 cat $var.harvest.txt | grep '@' > $var.email.txt
 cat $var.harvest.txt | grep ':' | sort | cut -d ':' -f 2 > $var.domains.txt
 cat $var.harvest.txt | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq | tee $var.ipaddress.txt
@@ -129,11 +131,11 @@ automater $var | tee $var.automater.txt
 #Active from here on BE FUCKING CAREFUL, fam
 cd ~/$var/active
 clear
-echo """
+echo """\e[31m 
 ========================================================================
    WARNING: ACTIVE SCANNING ONLY PERMITTED DURING CLIENT ENGAGEMENT 
 ========================================================================
-"""
+]"""
 read -p "Proceed with Active Scanning? (Y/N) "
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -149,7 +151,7 @@ fi
 ~/chktor.sh
 
 #run a few of nmap's greatest hits
-torsocks --shell | nmap -vv -F -Pn -sS -O --top-ports 50 -iL ~/$var/passive/$var.addr.txt --oA $var.opsys &&
+nmap -vv -F -Pn -sS -O --top-ports 50 -iL ~/$var/passive/$var.addr.txt --oA $var.opsys &&
 nmap -vv -6 -F -Pn -sS -O --top-ports 50 -iL ~/$var/passive/$var.addr.txt --oA $var.opsysIP6 &&
 nmap -vv -Pn -sS -sV -T3 -iL ~/$var/passive/$var.addr.txt --oA $var.sysserv &&
 nmap --script /usr/share/nmap/scripts/vulscan/vulscan.nse --oA $var.vulscan -iL ~/$var/passive/$var.addr.txt &&
@@ -157,8 +159,8 @@ nmap --script /usr/share/nmap/scripts/http-enum.nse --oA $var.http $var -iL ~/$v
 nmap --script /usr/share/nmap/scripts/smb-enum-domains.nse --oA $var.smb -iL ~/$var/passive/$var.addr.txt &&
 nmap --script /usr/share/nmap/scripts/ms-sql-info.nse -iL ~/$var/passive/$var.addr.txt --oA $var.mysqlinfo &&
 
-proxychains wpscan $var -y | tee $var.wpscan.txt
-proxychains ike-scan -A $var | tee $var.ike-scan.txt
+wpscan $var -y | tee $var.wpscan.txt
+ike-scan -A $var | tee $var.ike-scan.txt
 #sqlmap -a -u $var -y > $var.sqlmap.txt
 #nikto -host $var -output $var.nikto
 #golismero $var -o $var-golis.txt
